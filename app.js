31a19f9e1e102c96856b4a93a2e16f74f749622e
mehak kaur <mehakkaurmk21@gmail.com>
2022-10-08 21:58:07 -0400

    Assignment-1

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'node_modules' )));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,7 +36,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: 'Error' });
+  res.render('error', {title: 'Error'});
 });
 
 module.exports = app;
