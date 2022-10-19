5e70bc10880bfe181fab83effa1a22aed1c26e94
mehak kaur <mehakkaurmk21@gmail.com>
2022-10-19 16:40:21 -0400

    adding data to buisness svhema using form and feching it to the buisness contact form

--- controller/businessContacts.js
+++ controller/businessContacts.js
@@ -1,22 +1,48 @@
 const BusinessContact = require('../Models/businessContacts');
+
 exports.getBusinessContacts = (req, res, next) => {
-  res.render('authorised/businessContacts', {
-    title: 'Business Contacts',
-    message:'HEY'
-  });
+  let message = req.flash('contactDeleted');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  BusinessContact.find()  // FETCHING CONTACTS FROM BUSINESS CONTACTS COLLECTION
+    .collation({ locale: 'en', strength: 2 }).sort({ name: 1 }) // SORTING IN ALPHABETICAL ORDER 
+    .then(contacts => {
+      res.render('authorised/businessContacts', {
+        title: 'Business Contacts',
+        contacts: contacts,
+        message: message
+      });
+    })
+
 }
-exports.postContact=(req,res,next)=>{
+exports.postContact = (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const contactNumber = req.body.contactNumber;
-  const additionalInfo= req.body.additionalInfo;
+  const additionalInfo = req.body.additionalInfo;
   const businessContacts = new BusinessContact({
-    name:name,
-    email:email,
-    contactNumber:contactNumber,
-    description:additionalInfo
+    name: name,
+    email: email,
+    contactNumber: contactNumber,
+    description: additionalInfo
   })
   businessContacts.save();
-  console.log(name,email,contactNumber,additionalInfo);
+  console.log(name, email, contactNumber, additionalInfo);
   res.redirect('/contact');
+}
+
+exports.deleteUser = (req, res, next) => {
+  const userId = req.params.userId;
+  BusinessContact.findByIdAndRemove(userId)
+    .then(result => {
+      console.log(result);
+      req.flash('contactDeleted', 'Contact Deleted Successfully !!');
+      res.redirect('/businessContacts');
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
--- routes/businessContacts.js
+++ routes/businessContacts.js
@@ -5,5 +5,6 @@ const isAuth=require('../middelwear/is-auth');
 router.get('/businessContacts',isAuth,businessContactsController.getBusinessContacts);
 
 router.post('/submitContact',businessContactsController.postContact);
-
+router.get('/deleteUser/:contactId',businessContactsController.deleteUser);
+router.get('/edit-user/:contactId',businessContactsController.deleteUser);
 module.exports=router;
